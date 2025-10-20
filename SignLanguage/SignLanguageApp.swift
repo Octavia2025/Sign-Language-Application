a5a90cdbfbbafe7ae8ca720fe1a3f1ba93b5168f
Octavia <octaviatmucheche@gmail.com>
2025-10-20 11:38:08 +0200

    Initial Commit

---
+++ SignLanguage/SignLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageApp.swift
+//  SignLanguage
+//
+//  Created by Octavia Mucheche on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SignLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SignLanguageTests/SignLanguageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageTests.swift
+//  SignLanguageTests
+//
+//  Created by Octavia Mucheche on 10/20/25.
+//
+
+import Testing
+@testable import SignLanguage
+
+struct SignLanguageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
