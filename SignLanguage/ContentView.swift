d9a0496988685dd06241d57c3743bdf70cf7ab78
Octavia <octaviatmucheche@gmail.com>
2025-10-20 12:55:48 +0200

    welcome page

--- SignLanguage/ContentView.swift
+++ SignLanguage/ContentView.swift
@@ -4,18 +4,43 @@
 //
 //  Created by Octavia Mucheche on 10/20/25.
 //
-
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
         VStack {
             
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .padding()
+
+            
+            ZStack {
+                
+                HStack(spacing: 0) {
+                    Rectangle()
+                        .fill(Color.blue.opacity(0.3))
+                    Rectangle()
+                        .fill(Color.cyan.opacity(0.3))
+                }
+                
+                
+                HStack {
+                    Image("k1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .padding()
+
+                    Image("k2")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .padding()
+                }
+            }
+            .frame(height: 300)
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
