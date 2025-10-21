cfca34c761d27993c686b1e446923137f0b4740c
Octavia <octaviatmucheche@gmail.com>
2025-10-21 10:38:32 +0200

    Import AVKit

---
+++ SignLanguage/AnimalsFoodNature.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalsFoodNature.swift
+//  SignLanguage
+//
+//  Created by Octavia Mucheche on 10/21/25.
+//
+
+import SwiftUI
+
+struct AnimalsFoodNature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalsFoodNature()
+}
