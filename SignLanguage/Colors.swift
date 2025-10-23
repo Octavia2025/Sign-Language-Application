4dc48bf9abd7e37c8bf3b382092caffc9402086e
Octavia <octaviatmucheche@gmail.com>
2025-10-23 17:15:47 +0200

    deleted some colors

---
+++ SignLanguage/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  SignLanguage
+//
+//  Created by Octavia Mucheche on 10/23/25.
+//
+
+import SwiftUI
+
+struct Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Colors()
+}
